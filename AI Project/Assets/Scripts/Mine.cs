2cedaef29db41c8a6603dae0c5f58f7262d6c65b
Doggo499 <84175344+Doggo499@users.noreply.github.com>
2023-12-14 13:59:01 +0100

    Working on mine

--- AI Project/Assets/Scripts/Mine.cs
+++ AI Project/Assets/Scripts/Mine.cs
@@ -1,41 +1,122 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Mine : MonoBehaviour
 {
     public float explosionRadius = 5f;
     public int damage = 50;
+    public float delay = 2f; // Delay before the mine explodes
+    public MeshRenderer mineMeshRenderer;
+    public AudioSource beepSound;
+    public ParticleSystem explosionParticlesPrefab;
+
+    public AudioClip regularBeepClip;
+    public AudioClip fastBeepClip;
+    public AudioClip explosionClip;
 
     private bool exploded = false;
 
+    void Start()
+    {
+        // Disable the mesh renderer initially
+        if (mineMeshRenderer != null)
+        {
+            mineMeshRenderer.enabled = false;
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        if (!exploded && other.CompareTag("Enemy"))
+        // Trigger the explosion immediately if an enemy enters the trigger zone
+        if (!exploded && other.CompareTag("Zombie"))
         {
-            Explode();
+            CancelInvoke(); // Cancel the scheduled explosion
+            Invoke(nameof(Explode), delay);
+            StartCoroutine(BlinkMesh());
+        }
+    }
+
+    IEnumerator BlinkMesh()
+    {
+        float blinkInterval = 0.2f; // Initial blink interval
+        float timeToExplode = delay;
+
+        while (!exploded && timeToExplode > 0)
+        {
+            if (mineMeshRenderer != null)
+            {
+                mineMeshRenderer.enabled = !mineMeshRenderer.enabled;
+            }
+
+            if (beepSound != null)
+            {
+                if (timeToExplode <= 0.5f)
+                {
+                    beepSound.clip = fastBeepClip;
+                }
+                else
+                {
+                    beepSound.clip = regularBeepClip;
+                }
+
+                beepSound.Play();
+            }
+
+            yield return new WaitForSeconds(blinkInterval);
+
+            // Adjust the blink interval dynamically based on time left to explode
+            timeToExplode -= blinkInterval;
+
+            // Make the last 500ms of the beep go really fast
+            if (timeToExplode <= 0.5f)
+            {
+                blinkInterval = Mathf.Clamp(blinkInterval * 0.8f, 0.05f, 0.1f); // Increase speed during the last 500ms
+            }
+            else
+            {
+                blinkInterval = Mathf.Clamp(blinkInterval * 0.9f, 0.1f, 1.0f); // Regular speed before the last 500ms
+            }
         }
     }
 
     void Explode()
     {
+        if (exploded) return; // Ensure the mine only explodes once
         exploded = true;
 
-        // Play explosion sound or visual effects
+        // Stop blinking and hide the mesh
+        if (mineMeshRenderer != null)
+        {
+            mineMeshRenderer.enabled = false;
+        }
+
+        // Play final explosion sound
+        if (beepSound != null)
+        {
+            beepSound.clip = explosionClip;
+            beepSound.Play();
+        }
+
+        // Instantiate and play explosion particle effect
+        if (explosionParticlesPrefab != null)
+        {
+            ParticleSystem explosionParticles = Instantiate(explosionParticlesPrefab, transform.position, Quaternion.identity);
+            explosionParticles.Play();
+        }
 
         // Detect enemies in the explosion radius
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
 
         foreach (Collider hit in colliders)
         {
-            if (hit.CompareTag("Enemy"))
+            if (hit.CompareTag("Zombie"))
             {
                 // Deal damage to enemies
-                //EnemyHealth enemyHealth = hit.GetComponent<EnemyHealth>();
-                //if (enemyHealth != null)
-                //{
-                //    enemyHealth.TakeDamage(damage);
-                //}
+                ZombieHealth enemyHealth = hit.GetComponent<ZombieHealth>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.health -= damage;
+                }
             }
         }
 
